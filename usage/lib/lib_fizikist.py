d9884a1fb903cba63b87a42f650b53064b93fd8a
Ebrar <ebrar.sahin@stu.fsm.edu.tr>
2018-02-22 23:56:23 +0300

    analyze website content

--- usage/lib/lib_fizikist.py
+++ usage/lib/lib_fizikist.py
@@ -18,24 +18,24 @@ if num_files == 0:
 print num_files
 
 #write to file
-file_html = open("/Users/ebrarsahin/GitHub/Graduation-Project/file/html"+str((num_files/2))+".txt", "a")
+file_html = open("/Users/ebrarsahin/GitHub/Graduation-Project/file/html"+str((num_files))+".txt", "w")
 file_html.write(html)
 file_html.close()
-
-#print (html)
-
+#
+# #print (html)
+#
 #get rid of html tags
 
 soup = BeautifulSoup(html, "html5lib")
 
 text = soup.get_text(strip=True)
 text=text.encode('utf-8').decode('ascii','ignore')
-file_text = open("/Users/ebrarsahin/GitHub/Graduation-Project/file/text"+str((num_files/2))+".txt", "a")
+file_text = open("/Users/ebrarsahin/GitHub/Graduation-Project/file/text"+str((num_files/2))+".txt", "w")
 file_text.write(text)
 file_text.close()
 
 #print(text)
-file_tokens = open("/Users/ebrarsahin/GitHub/Graduation-Project/file/tokens"+str((num_files/2))+".txt", "a")
+file_tokens = open("/Users/ebrarsahin/GitHub/Graduation-Project/file/tokens"+str((num_files/2))+".txt", "w")
 
 for t in text.split():
     print (t)
@@ -46,9 +46,11 @@ tokens = [t for t in text.split()]
 freq = nltk.FreqDist(tokens)
 
 for key, val in freq.items():
-    file_token_freq = open("/Users/ebrarsahin/GitHub/Graduation-Project/file/token_freq" + str((num_files / 2)) + ".txt", "a")
+    file_token_freq = open("/Users/ebrarsahin/GitHub/Graduation-Project/file/token_freq" + str((num_files / 2)) + ".txt", "w")
     file_token_freq.write(str(key) + ':' + str(val) + '\n')
     print (str(key) + ':' + str(val))
 file_token_freq.close()
+
+
 
 freq.plot(20, cumulative=False)
