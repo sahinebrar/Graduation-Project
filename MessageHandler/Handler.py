6b11e0b0b03a54ca91477c1407a3498421bdc937
Ebrar <ebrar.sahin@stu.fsm.edu.tr>
2018-03-25 16:13:10 +0300

    updated a function for processing

--- MessageHandler/Handler.py
+++ MessageHandler/Handler.py
@@ -1,6 +1,20 @@
+from fuzzywuzzy import fuzz
+from MessageHandler import CorporaToLists as cpr
+import random
+
 
 def substringReceivedMessage(str=None):
     return None
 
+
 def process_the_message(msg):
-    return "chatbot: "+msg
+    global matched
+    matched = False
+    word_list = cpr.convert_corpora_to_list()
+    for i in word_list:
+        print(str(fuzz.partial_ratio(i, msg)))
+        if fuzz.partial_ratio(i, msg) >= 70:
+            matched = True
+            return "chatbot: " + random.choice(word_list)
+    if matched is False:
+        return "chatbot: " + "ne dediğinizi anlayamadım."
